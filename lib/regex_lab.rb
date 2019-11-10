df49429d5aa1971911bf54c7c8e12bcaaac5852a
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-10 23:00:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,10 @@
 def starts_with_a_vowel?(word)
-  array=[]
-array.scan(/\w + [aeiou]/)
+  if word.match(/^[AEIOU]|^[aeiou]/)
+      true
+    else
+      false
+    end
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
